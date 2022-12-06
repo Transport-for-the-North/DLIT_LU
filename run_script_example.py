5fb7985f291c9e56808d555f0689790026c43a62
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2022-12-06 16:38:30 +0000

    Example setup

---
+++ run_script_example.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+""" """
+# Built-Ins
+
+# Third Party
+
+# Local Imports
+# pylint: disable=import-error,wrong-import-position
+from src.DLIT_LU import example
+
+# pylint: enable=import-error,wrong-import-position
+
+# # # CONSTANTS # # #
+
+# # # CLASSES # # #
+
+
+# # # FUNCTIONS # # #
+def main():
+    example.do_something()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/DLIT_LU/example.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+""" """
+# Built-Ins
+import logging
+
+# Third Party
+
+# Local Imports
+# pylint: disable=import-error,wrong-import-position
+
+# pylint: enable=import-error,wrong-import-position
+
+# # # CONSTANTS # # #
+LOG = logging.getLogger(__name__)
+
+# # # CLASSES # # #
+
+
+# # # FUNCTIONS # # #
+def do_something():
+    print("Hello world")
