2d9f01ed8ab8a9bf702f05bc2145c520324af2b4
kgouderkotresh <kgouderkotresh@systra.com>
2025-03-04 09:57:11 +0000

    constraint and viz added

--- src/dlit_lu/constraint.py
+++ src/dlit_lu/constraint.py
@@ -1,13 +1,3 @@
-# load ddg_data, dlog (lad)():
-# aggregate(): from lad to region - ddg and dlog.  Should have region and lad data. 8 Datasets - 4 in lad for ddg and dlog and 4 region ddg and region dlog
-# assign zone id to name(): 
-# def calculate growth rate(): 
-# merge ddg and dlog(): pop and emp , 4 dataframes 
-# visualise(): pop and emp 4 visuals 
-# save in 06>visuals , 06>output 
-
-# lad_population and lad_job call these 
-
 import pandas as pd
 import pathlib
 import logging
@@ -17,6 +7,7 @@ import os
 from dlit_lu import inputs, utilities
 import numpy as np
 
+from dlit_lu.viz import GrowthRateVisualizer
 
 LOG = logging.getLogger(__name__)
 
@@ -97,7 +88,7 @@ class ConstraintProcessor():
                       name_column, 
                       use_region_name=False,
                       use_region_cols=False):
-        
+    
         # Determine which name file to use
         if use_region_name:
             name_file = self.config.constraint.region_name
@@ -107,11 +98,8 @@ class ConstraintProcessor():
         # Read the name file
         name_df = pd.read_csv(name_file)
 
-        # Convert zone_id to string to match lad2013_id
-        #name_df['zone_id'] = name_df['zone_id'].astype(str)
-
         if use_region_cols:
-        # Merge
+            # Merge
             data_with_name = data.merge(name_df, left_on=id_column, right_on='zone_id', how='left')
             data_with_name = data_with_name.drop(columns=['zone_id']) 
             data_with_name = data_with_name.rename(columns={id_column: 'REGIONCD', 'zone_name': 'REGIONNM'})
@@ -128,9 +116,71 @@ class ConstraintProcessor():
             columns.insert(1, 'LADNM')
             data_with_name = data_with_name[columns]
 
+            # Add region names to LAD data
+            if 'LAD13CD' in data_with_name.columns:
+                lad_to_region_df = pd.read_csv(self.config.dev_pattern.summary_data.lad_to_region_file)
+                region_name_df = pd.read_csv(self.config.constraint.region_name)
+                data_with_name = data_with_name.merge(lad_to_region_df[['lad2013_id', 'ntem_region_id']], 
+                                                  left_on='LAD13CD', right_on='lad2013_id', how='left')
+                data_with_name = data_with_name.merge(region_name_df[['zone_id', 'zone_name']], 
+                                                  left_on='ntem_region_id', right_on='zone_id', how='left')
+                data_with_name = data_with_name.rename(columns={'zone_name': 'REGIONNM'})
+                data_with_name = data_with_name.drop(columns=['lad2013_id', 'ntem_region_id', 'zone_id'])
+                cols = list(data_with_name.columns)
+                cols.insert(cols.index('LADNM') + 1, cols.pop(cols.index('REGIONNM')))
+                data_with_name = data_with_name[cols]
+
         LOG.info(f"Added {name_column} to dataset with shape {data_with_name.shape}")
 
         return data_with_name
+    
+    def combine_datasets(self, growth_rate_results):
+        """
+        Split datasets into DDG and DLOG and prepare them for saving.
+
+        Parameters
+        ----------
+        growth_rate_results : list of pd.DataFrame
+         List of processed datasets with growth rates calculated.
+
+        Returns
+        -------
+        dict
+            Dictionary containing datasets split into DDG and DLOG.
+        """
+    # Define the source names based on the order of datasets
+        source_names = ['DDG', 'DDG', 'DLOG', 'DLOG', 'DDG', 'DDG', 'DLOG', 'DLOG']
+    
+    # Add Source column to each dataset and reorder columns
+        for i, data in enumerate(growth_rate_results):
+            data['Source'] = source_names[i]
+        # Reorder columns to place 'Source' after 'LADNM' or 'REGIONNM'
+            if 'LADNM' in data.columns:
+                cols = list(data.columns)
+                cols.insert(cols.index('LADNM') + 1, cols.pop(cols.index('Source')))
+                data = data[cols]
+            elif 'REGIONNM' in data.columns:
+                cols = list(data.columns)
+                cols.insert(cols.index('REGIONNM') + 1, cols.pop(cols.index('Source')))
+                data = data[cols]
+            growth_rate_results[i] = data
+
+    # Split datasets into DDG and DLOG
+        ddg_pop = growth_rate_results[0]
+        dlog_pop = growth_rate_results[2]
+        ddg_emp = growth_rate_results[1]
+        dlog_emp = growth_rate_results[3]
+        region_ddg_pop = growth_rate_results[4]
+        region_dlog_pop = growth_rate_results[6]
+        region_ddg_emp = growth_rate_results[5]
+        region_dlog_emp = growth_rate_results[7]
+
+        return {
+            'LAD_Population': {'DDG': ddg_pop, 'DLOG': dlog_pop},
+            'LAD_Employment': {'DDG': ddg_emp, 'DLOG': dlog_emp},
+            'Region_Population': {'DDG': region_ddg_pop, 'DLOG': region_dlog_pop},
+            'Region_Employment': {'DDG': region_ddg_emp, 'DLOG': region_dlog_emp}
+        }
     
 def calculate_growth_rate(data, year_columns):
     """
@@ -146,7 +196,6 @@ def calculate_growth_rate(data, year_columns):
     growth_rate = data.copy()
     year_columns = sorted([int(year) for year in year_columns])
 
-    # List to keep track of created CAGR columns
     cagr_columns = []
 
     # Calculate CAGR for each period
@@ -160,15 +209,15 @@ def calculate_growth_rate(data, year_columns):
             start_value = row[str(start_year)]
             end_value = row[str(end_year)]
             if start_value == 0:
-                return None  # Skip calculation and return None for zero start value
+                return None  
             return ((end_value / start_value) ** (1 / years) - 1) * 100
 
         cagr_column_name = f'CAGR_{end_year}'
         growth_rate[cagr_column_name] = data.apply(calculate_row_growth, axis=1)
         cagr_columns.append(cagr_column_name)
 
-    # Select only the first column and existing CAGR columns
-    result = growth_rate.iloc[:, [0,1] + [growth_rate.columns.get_loc(col) for col in cagr_columns]]
+    # Select only the first columns and existing CAGR columns
+    result = growth_rate.iloc[:, [0, 1, 2] + [growth_rate.columns.get_loc(col) for col in cagr_columns]]
 
     return result
 
@@ -261,146 +310,15 @@ def run(config: inputs.DLitConfig):
         growth_rate_result = calculate_growth_rate(data, year_columns)
         growth_rate_results.append(growth_rate_result)
 
-    # Save results
-    filenames = [
-        "ddg_pop.csv", 
-        "ddg_emp.csv", 
-        "dlog_population.csv",
-        "dlog_employment.csv",
-        "region_ddg_pop.csv", 
-        "region_ddg_emp.csv", 
-        "region_dlog_pop.csv", 
-        "region_dlog_emp.csv"
-    ]
+    # Split and prepare datasets
+    prepared_datasets = processor.combine_datasets(growth_rate_results)
 
-    for filename, data in zip(filenames, growth_rate_results):
-        utilities.write_to_csv(key_constraint_path / filename, data)
+    for category, datasets in prepared_datasets.items():
+        excel_output_path = key_constraint_path / f'{category}.xlsx'
+        utilities.write_to_excel(excel_output_path, datasets)
 
-    LOG.info("Data processing and aggregation completed")
+    # Visualize the data
+    visualizer = GrowthRateVisualizer(output_dir=key_constraint_path / 'visualizations')
+    visualizer.generate_visualizations(prepared_datasets)
 
-# def merge():
-#     return
-
-# def visualise():
-#     return
-# def run(config:inputs.ConstraintsConfig):
-#     return
-
-
-
-# def merge_datasets(dataset1, dataset2, key_column1, key_column2, suffix1='_1', suffix2='_2', keep_key='left'):
-#     """
-#     Merge two datasets on specified key columns with suffixes and keep only one key column.
-
-#     Args:
-#         dataset1 (DataFrame): The first DataFrame to merge.
-#         dataset2 (DataFrame): The second DataFrame to merge.
-#         key_column1 (str): The key column name in the first DataFrame.
-#         key_column2 (str): The key column name in the second DataFrame.
-#         suffix1 (str): Suffix to apply to overlapping columns from the first DataFrame.
-#         suffix2 (str): Suffix to apply to overlapping columns from the second DataFrame.
-#         keep_key (str): Specify which key column to keep ('left' or 'right').
-
-#     Returns:
-#         DataFrame: A merged DataFrame with only one key column.
-#     """
-#     merged_data = dataset1.merge(
-#         dataset2,
-#         left_on=key_column1,
-#         right_on=key_column2,
-#         suffixes=(suffix1, suffix2),
-#         how='outer'
-#     )
-    
-#     if keep_key == 'left':
-#         if key_column2 in merged_data.columns:
-#             merged_data.drop(columns=[key_column2], inplace=True)
-#     elif keep_key == 'right':
-#         if key_column1 in merged_data.columns:
-#             merged_data.drop(columns=[key_column1], inplace=True)
-#     else:
-#         raise ValueError("Invalid value for 'keep_key'. Use 'left' or 'right'.")
-
-#     return merged_data
-
-# def visualize_results(data, output_file):
-#     """
-#     Visualize the results in an interactive HTML file.
-
-#     Args:
-#         data (DataFrame): The DataFrame containing growth rate data.
-#         output_file (str): The path to save the HTML visualization.
-
-#     Returns:
-#         None
-#     """
-#     fig = go.Figure()
-#     for zone in data['LAD13CD'].unique():
-#         zone_data = data[data['LAD13CD'] == zone]
-#         for col in zone_data.columns:
-#             if col.startswith('growth_'):
-#                 year = col.split('_')[1]
-#                 trace = go.Scatter(
-#                     x=[year], y=zone_data[col], mode='lines+markers',
-#                     name=f"{zone} - {year}", hovertemplate='%{y:.2f}%'
-#                 )
-#                 fig.add_trace(trace)
-#     fig.update_layout(
-#         title="Growth Rate Visualization",
-#         xaxis_title="Year",
-#         yaxis_title="Growth Rate (%)",
-#         yaxis_tickformat=",d"
-#     )
-#     fig.write_html(output_file)
-#     print(f"Visualization saved as: {output_file}")
-
-
-# # if __name__ == "__main__":
-# #     # Specify input file paths
-# #     lad_population_file_path = r"I:\NorMITs Demand\import\edge_replicant\edge_inputs\Drivers\Jan 24 central DDG drivers\DD_Jan24_Central_Emp_LA.csv"
-# #     #  = "path/to/lad_population.csv"
-
-# #         # Load data
-# #     lad_population = pd.read_csv(lad_population_file_path)
-# #     # lad_population = pd.read_csv(lad_population_file_path)
-
-# #     # Determine year columns from lad_population
-# #     year_columns = [col for col in lad_population.columns if col.isdigit()]
-
-# #     # Calculate growth rates
-# #     # ddg_pop_growth = calculate_growth_rate(ddg_pop_lad, year_columns)
-# #     lad_population_growth = calculate_growth_rate(lad_population, year_columns)
-
-
-
-# #     # ddg_pop_growth.to_csv("ddg_pop_growth_rates.csv", index=False)
-
-# #     out = "I:\\Data\\D-Log\\DLIT\\Outputs\\test10\\05_normits"
-# #     lad_population_growth.to_csv(os.path.join(out, "ddg_population_growth_rates.csv"), index=False)
-
-#     ######## Growth Rate ######
-
-#     LOG.info("Calculating Growth Rate and Visualising results")
-
-#     # Determine year columns from lad_population or lad_job
-#     year_columns = [col for col in lad_population.columns if col.isdigit()]
-
-#     # Subset DDG datasets to match LAD datasets
-#     ddg_pop_lad = ddg_pop_lad[['LAD13CD'] + year_columns]
-#     ddg_emp_lad = ddg_emp_lad[['LAD13CD'] + year_columns]
-
-#     #ddg_pop_lad.to_csv(os.path.join(key_output_path, "ddg_pop_lad.csv"))
-
-#     # Calculate growth rates
-#     ddg_pop_growth = calculate_growth_rate(ddg_pop_lad, year_columns)
-#     lad_population_growth = calculate_growth_rate(lad_population, year_columns)
-#     lad_job_growth = calculate_growth_rate(lad_job, year_columns)
-#     ddg_emp_growth = calculate_growth_rate(ddg_emp_lad, year_columns)
-
-#     merged_growth_data_pop = merge_datasets(lad_population_growth, ddg_pop_growth,key_column1='lad2013_id', key_column2='LAD13CD',suffix1='_lad', suffix2='_ddg',keep_key='right' ) 
-#     #merged_growth_data_pop.to_csv(os.path.join(key_output_path, "merged_growth_data_pop.csv"))
-#     merged_growth_data_job = merge_datasets(lad_job_growth, ddg_emp_growth,key_column1='lad2013_id', key_column2='LAD13CD',suffix1='_lad', suffix2='_ddg',keep_key='right')
-
-#     #os.makedirs(output_directory, exist_ok=True)
-#     visualize_results(merged_growth_data_pop, os.path.join(key_output_path, "growth_rate_visualization_pop.html"))
-#     visualize_results(merged_growth_data_job, os.path.join(key_output_path, "growth_rate_visualization_job.html"))
+    LOG.info("Data processing, aggregation, and visualization completed")
