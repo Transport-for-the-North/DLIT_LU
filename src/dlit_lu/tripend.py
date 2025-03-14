f568d17ec26a878fea8011c2f166d73f5056c1c8
kgouderkotresh <kgouderkotresh@systra.com>
2025-03-14 09:26:42 +0000

    adding tem

--- src/dlit_lu/tripend.py
+++ src/dlit_lu/tripend.py
@@ -2,9 +2,12 @@ import pandas as pd
 import logging
 from typing import Dict, Any
 import numpy as np
+from pathlib import Path
 
 # Local imports
 from dlit_lu import inputs, utilities
+
+import caf.tem as ct
 
 LOG = logging.getLogger(__name__)
 
@@ -17,6 +20,31 @@ class Tripends():
         print(tt)
         
         return soc_sic, tt  
+
+    def tem():
+        
+        tem = ct.TEM(
+            model_years=[2023],
+            cenario="Core",
+            output_zoning="normits",
+            iteration_name="20250310",
+            export_home=r"T:\ThomasPrince\TEM I-Drive Comparison\Outputs - caf.tem",
+            return_segmentation=["hh_type", "p", "m", "tp"]
+            )
+
+        input_dir = Path(r"T:\ThomasPrince\TEM I-Drive Comparison\Inputs\01-HBProduction")
+
+        HBProd = tem.HBProductionModel(
+            population_paths={2023: input_dir / "lu_pop_2023.hdf"},
+            trip_rates_path=input_dir / "hb_trip_rates_production.hdf",
+            mode_time_splits_path=input_dir / "mode_time_split_production_hb_fr_reg.hdf",
+            adjustment_path=input_dir / "trip_rate_adjustments_production_hb_fr.hdf",
+            mts_adj_path=r"T:\ThomasPrince\TEM I-Drive Comparison\Inputs\01-HBProduction\mode_time_split_adjustments.hdf",
+            population_translation_path=r"T:\ThomasPrince\TEM I-Drive Comparison\Inputs\normits_lsoa_2021_pop.csv"
+            )
+
+        HBProd.run()
+
 
 def run(config: inputs.DLitConfig):
 
